f9b27b2a119b08547b17dd29ab015a2e8b6d0c5f
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-03 16:07:15 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+//event listner to detect when DOM is loaded
+//the listners callback 
+//  to target id= text
+//  replace text with "this is really cool"
+
+
+document.addEventListener(
+    "DOMContentLoaded", 
+    function() {
+        
+        const word2 = document.getElementById('text');
+        
+        word2.textContent = ('This is really cool!');
+        
+        word2.addEventListener('load', () => word2.textContent = ('This is really cool!'))
+
+});
